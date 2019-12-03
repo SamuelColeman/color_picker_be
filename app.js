881365823cc1ad764023016cc406d41c58c7fa99
Samuel Coleman <smcoleman19@gmail.com>
2019-12-03 16:15:26 -0700

    Add testing and endpoint for getting all projects

--- app.js
+++ app.js
@@ -13,4 +13,10 @@ app.get('/', (request, response) => {
 	response.send('Palette Picker BE is running!')
 });
 
+app.get('/api/v1/projects', (request, response) => {
+	database('projects').select()
+		.then(projects => response.status(200).json(projects))
+		.catch(error => response.status(500).json({ error }))
+});
+
 module.exports = app;
